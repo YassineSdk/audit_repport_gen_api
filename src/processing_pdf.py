c28bcd7c5cf3e57bb40a0b0b5784edaba67b1518
YassineSdk <yassine.sadiki2-etu@etu.univh2c.ma>
2026-03-09 13:08:26 +0000

    starting new project

---
+++ src/processing_pdf.py
@@ -0,0 +1,22 @@
+import fitz 
+import os
+
+SUPPORTED_TYPE="application/pdf"
+
+def exctract_text_from_pdf(file_path: bytes)->str:
+    """Extract raw text from a PDF file , page by page"""
+
+    doc = fitz.open(file_path)
+    collection = []
+    for page_num, page in enumerate(doc, start=1):
+        text = page.get_text("text")
+        if text.strip():
+            collection.append(f"---Page {page_num} ---\n{text.strip()}")
+    doc.close()
+    if not collection:
+        raise ValueError("No text could be extrated from this doc ")
+
+    return "\n\n".join(collection)
+
+text = exctract_text_from_pdf("audit_report.pdf")
+print(text)
